b5eccfd7240fb6d1b772c4a606426ec59140cc45
brisa scomazzon <analuzespindolapagano@gmail.com>
2026-06-04 18:23:24 -0300

    esta en rango

--- homework2.js
+++ homework2.js
@@ -63,6 +63,12 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+if (numero <50 && numero > 20){
+    return "true";
+  }else {
+    return "false";
+  }
+
 
 }
 
