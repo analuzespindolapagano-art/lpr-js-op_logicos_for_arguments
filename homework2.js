c98632cb2488b78a2aa5d243f7b8caef37fb540c
brisa scomazzon <analuzespindolapagano@gmail.com>
2026-06-04 18:37:16 -0300

    es primo

--- homework2.js
+++ homework2.js
@@ -113,7 +113,12 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-
+for (let i=2; numero > i; i++)
+  if (numero % i === 0) {
+  return "true";
+}else{
+  return "false";
+}
 
 
 }
