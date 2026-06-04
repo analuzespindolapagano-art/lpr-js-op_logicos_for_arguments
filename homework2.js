a83aeacace330561eead7eda0efc0e5a7e235d52
brisa scomazzon <analuzespindolapagano@gmail.com>
2026-06-04 18:31:13 -0300

    es entero

--- homework2.js
+++ homework2.js
@@ -80,7 +80,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-
+if (numero === Math.floor(numero)){
+  return "true";
+}else {
+  return "false";
+}
 }
 
 function fizzBuzz(numero) {
